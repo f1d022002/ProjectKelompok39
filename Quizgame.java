a12c1911c2d0e346fa6fbb3a45326f2d1e67e945
zhambiya27 <zambiya4@gmail.com>
2024-12-07 23:24:40 +0800

    update-quizgame

--- Quizgame.java
+++ Quizgame.java
@@ -1,34 +1,19 @@
-import javax.swing.*;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Quizgame {
-    private static final String SCORE_FILE = "scores.txt";
-
     public static void main(String[] args) {
-        // Buat instance ScoreManager untuk menyimpan dan membaca skor
-        ScoreManager scoreManager = new ScoreManager(SCORE_FILE);
-
-        // Tampilkan menu utama
+        // Inisialisasi antarmuka pengguna (GameBoard)
         SwingUtilities.invokeLater(() -> {
-            Menu menu = new Menu();
-            menu.startButton.addActionListener(e -> {
-                // Mulai permainan
-                menu.dispose();
-                SwingUtilities.invokeLater(() -> new ShapeQuiz(scoreManager));
-            });
+            try {
+                // Atur tampilan dan nuansa (Look and Feel) untuk antarmuka
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-            menu.scoreButton.addActionListener(e -> {
-                // Tampilkan skor
-                JOptionPane.showMessageDialog(menu,
-                        scoreManager.getScores(),
-                        "Daftar Skor",
-                        JOptionPane.INFORMATION_MESSAGE);
-            });
-
-            menu.exitButton.addActionListener(e -> {
-                // Keluar dari aplikasi
-                System.exit(0);
-            });
+            // Jalankan game board
+            new GameBoard();
         });
-
     }
 }
